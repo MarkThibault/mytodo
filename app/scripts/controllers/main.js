cb76fbcdac66258e5a19cf52a444c3cad79de7bc
Mark Thibault <mthibault03@gmail.com>
2014-07-15 17:23:36 -0700

    Initial Build

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mytodoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mytodoApp
+ */
+angular.module('mytodoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    $scope.todos = ['Item1', 'Item2', 'Item3'];
+  });
